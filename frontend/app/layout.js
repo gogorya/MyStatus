ba147b68164c840672f8c3fda030cd34824045b4
gogorya <nimitgogorya1998@gmail.com>
2025-01-02 14:00:55 +0530

    Integrated clerk authentication on frontend.

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -11,18 +12,20 @@ const inter = Inter({
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      suppressHydrationWarning
-      className={`${inter.variable} antialiased`}
-    >
-      <body className="flex flex-col h-screen">
-        <ThemeProvider>
-          <Navbar />
-          <main className="flex-1">{children}</main>
-          <Footer />
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html
+        lang="en"
+        suppressHydrationWarning
+        className={`${inter.variable} antialiased`}
+      >
+        <body className="flex flex-col h-screen">
+          <ThemeProvider>
+            <Navbar />
+            <main className="flex-1">{children}</main>
+            <Footer />
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -1,3 +1,15 @@
+import { SignedIn, UserButton, OrganizationSwitcher } from "@clerk/nextjs";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <div>Navbar</div>
+      <div>
+        <SignedIn>
+          <OrganizationSwitcher />
+          <UserButton />
+        </SignedIn>
+      </div>
+    </div>
+  );
 }
